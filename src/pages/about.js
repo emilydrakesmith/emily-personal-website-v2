b42c4248b93536946b6539085c4b0c9449898279
mhsmith321 <emily.drake.smith@gmail.com>
2024-01-08 09:36:55 -0500

    add new content to `about.js` file

--- src/pages/about.js
+++ src/pages/about.js
@@ -34,11 +34,14 @@ export default function About() {
         )
     };
 
-    const intro = <p>Hi there!  I'm Emily (she + her + hers) and I've been building things for the internet since the days of BBCode and Geocities in the early 00s.  It's been a long time since then and now I'm a software engineer with {links.crocLabs}, a start-up building a new DeFi cryptocurrency exchange.<br/><br/>
-    My work is something in which I take great pride.  While no detail is unimportant, I particularly care about designs being clean and intuitive, code that's efficient and modular, and extensive documentation.<br/><br/>
-    Outside of work I love spending time with my wife.  We greatly enjoy playing board games, camping in our trailer, cooking, the New York Rangers, fishing, and wine tasting.<br/><br/>
-    Please look around this site to get to know me better.  I'm currently quite happily employed but always interested in making new network contacts or discovering future opportunities. I'm also available for speaking engagements of varying formats.<br/><br/>
-    I'm a proud alumnus of {links.stockton}, {links.ga}, and the {links.mast}.  I'm always up for connecting with other alumni or current students from the places I've been; feel free to reach out if that's you!</p>;
+    const lineBreak = <><br/><br/></>;
+
+    const intro = <p>Hi there!  I'm Emily (she + her + hers) and I've been building things for the internet since the days of BBCode and Geocities in the early 00s.  It's been a long time since then and now I'm a software engineer with {links.crocLabs}, a start-up building a new DeFi cryptocurrency exchange.{lineBreak}
+    My work is something in which I take great pride.  While no detail is unimportant, I particularly care about designs being clean and intuitive, code that's efficient and modular, and extensive documentation.{lineBreak}
+    Outside of work I love spending time with my wife.  We greatly enjoy playing board games, camping in our trailer, cooking, the New York Rangers, fishing, and wine tasting.{lineBreak}
+    Please look around this site to get to know me better.  I'm currently quite happily employed but always interested in making new network contacts or discovering future opportunities. I'm also available for speaking engagements of varying formats.{lineBreak}
+    I'm a proud alumnus of {links.stockton}, {links.ga}, and the {links.mast}.  I'm always up for connecting with other alumni or current students from the places I've been; feel free to reach out if that's you!{lineBreak}
+    Science is real. Love is beatiful. Black lives matter. LGBTQ+ rights are human rights. No human is illegal. Trans men are men. Trans women are women. Protect trans kids.</p>;
 
     const skillsDesktop = 
         <>
